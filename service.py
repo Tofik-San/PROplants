84b35f51bb53d5a5aba1285a0173887c3682977d
Tofik-San <Tofkazlo@Gmail.com>
2025-06-20 20:26:08 +0300

    Update service.py

--- service.py
+++ service.py
@@ -1,1 +1,17 @@
+# service.py
+import bentoml
+from bentoml.io import JSON
+from gpt_module import generate_gpt_response
+
+svc = bentoml.Service("plants_gpt_service")
+
+@svc.api(input=JSON(), output=JSON())
+def chat(data):
+    template = data.get("template", "")
+    detail = data.get("detail", "")
+    task = data.get("task", "")
+    goal = data.get("goal", "")
+
+    result = generate_gpt_response(template, detail, task, goal)
+    return {"response": result}
 
